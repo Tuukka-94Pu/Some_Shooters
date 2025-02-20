fee9ab0911a5d8107b933516e3d526fecc4cf718
Tuukka-94Pu <tuukka.toivola@vuoksi.fi>
2025-02-20 10:08:05 +0200

    Shooting (non functional)

---
+++ Assets/Tuukka/T's Scripts/T_EnemyBeans.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class T_EnemyBeans : MonoBehaviour
+{
+
+
+    public float health = 100.0f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TakeDamage(float damageAmount)
+
+    {
+
+        health -= damageAmount;
+
+        Debug.Log("Enemy health is: " + health);
+
+        if (health <= 0)
+
+        {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+}
+
