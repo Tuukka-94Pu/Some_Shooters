2f1224cad74d1682efa1771f23f3ca06247a779c
henry-9ga <henry.jarvenpaa@vuoksi.fi>
2025-02-20 15:32:29 +0200

    Added shooting
    Doesnt work yet

--- Assets/Henry/HJ_Scripts/HJ_CameraMovement.cs
+++ Assets/Henry/HJ_Scripts/HJ_CameraMovement.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 
-public class HJ_CameraMovement : MonoBehaviour
+namespace HJ
 {
-    public float mouseSensitivity = 100.0f;
-    private Transform playerBody;
-    private float xRotation = 0.0f;
-
-    private void Start()
+    public class HJ_CameraMovement : MonoBehaviour
     {
-        playerBody = transform.parent;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        public float mouseSensitivity = 100.0f;
+        private Transform playerBody;
+        private float xRotation = 0.0f;
 
-    private void Update()
-    {
-        RotateCamera();
-    }
+        private void Start()
+        {
+            playerBody = transform.parent;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
-    private void RotateCamera()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        playerBody.Rotate(Vector3.up * mouseX);
+        private void Update()
+        {
+            RotateCamera();
+        }
+
+        private void RotateCamera()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
     }
 }
